46589d9649519ddf780b1017ce08fc753cf06e76
VladislavPetyukevich <pet.vladislav@gmail.com>
2022-02-15 21:42:31 +0300

    Rotate cylinder to show number

--- source/scenes/testScene.ts
+++ source/scenes/testScene.ts
@@ -8,6 +8,7 @@ import {
 } from 'three';
 import { BasicSceneProps, BasicScene } from '@/core/Scene';
 import slotBackground from '@/assets/slot.png';
+import numberRoll from '@/assets/number-roll.png';
 
 export interface TestSceneProps extends BasicSceneProps {
 }
@@ -19,10 +20,12 @@ export class TestScene extends BasicScene {
   ambientLight: AmbientLight;
   ambientLightColor: number;
   ambientLightIntensity: number;
+  rotationXShiftDegrees: number;
 
   constructor(props: TestSceneProps) {
     super(props);
 
+    this.rotationXShiftDegrees = 18;
     this.ambientLightColor = 0xFFFFFF;
     this.ambientLightIntensity = 7;
     this.ambientLight = new AmbientLight(
@@ -32,13 +35,21 @@ export class TestScene extends BasicScene {
     this.scene.add(this.ambientLight);
 
     this.cylinder1 = new Mesh(
-      new CylinderGeometry(),
-      new MeshLambertMaterial({ wireframe: true, color: 0x00ff00 }),
+      new CylinderGeometry(1, 1, 1, 32),
+      new MeshLambertMaterial(),
     );
-    this.cylinder1.rotation.x = Math.PI / 2;
+    this.cylinder1.rotation.x = this.getCylinderRotationToNumber(7);
     this.cylinder1.rotation.z = Math.PI / 2;
     this.scene.add(this.cylinder1);
     this.camera.position.z = 5;
+
+    loader.load(
+      numberRoll,
+      (texture) => {
+        (<MeshLambertMaterial>this.cylinder1.material).map = texture;
+        (<MeshLambertMaterial>this.cylinder1.material).needsUpdate = true;
+      },
+    );
 
     loader.load(
       slotBackground,
@@ -52,7 +63,18 @@ export class TestScene extends BasicScene {
     );
   }
 
+  getCylinderRotationToNumber(number: number) {
+    const oneNumberDegrees = 360 / 10;
+    return this.toRadians(
+      oneNumberDegrees * number +
+      this.rotationXShiftDegrees
+    );
+  }
+
   update(delta: number) {
-    this.cylinder1.rotateY(-delta * 1);
+  }
+
+  toRadians(angle: number) {
+    return angle * (Math.PI / 180);
   }
 }
