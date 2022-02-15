2b8c31e8eeee28de76425750a5838d72c4a7e1bc
VladislavPetyukevich <pet.vladislav@gmail.com>
2022-02-15 20:57:50 +0300

    Slot background texture

--- source/scenes/testScene.ts
+++ source/scenes/testScene.ts
@@ -1,38 +1,55 @@
 import {
-  PlaneGeometry,
   Mesh,
-  PointLight,
-  Matrix4,
-  MeshLambertMaterial,
-  Vector2,
-  Vector3,
-  Fog,
-  AmbientLight,
-  RepeatWrapping,
-  Color,
   CylinderGeometry,
-  PerspectiveCamera,
   BoxGeometry,
-  MeshBasicMaterial,
+  MeshLambertMaterial,
+  TextureLoader,
+  AmbientLight,
 } from 'three';
 import { BasicSceneProps, BasicScene } from '@/core/Scene';
+import slotBackground from '@/assets/slot.png';
 
 export interface TestSceneProps extends BasicSceneProps {
 }
 
+const loader = new TextureLoader();
+
 export class TestScene extends BasicScene {
   cylinder1: Mesh;
+  ambientLight: AmbientLight;
+  ambientLightColor: number;
+  ambientLightIntensity: number;
 
   constructor(props: TestSceneProps) {
     super(props);
+
+    this.ambientLightColor = 0xFFFFFF;
+    this.ambientLightIntensity = 7;
+    this.ambientLight = new AmbientLight(
+      this.ambientLightColor,
+      this.ambientLightIntensity
+    );
+    this.scene.add(this.ambientLight);
+
     this.cylinder1 = new Mesh(
       new CylinderGeometry(),
-      new MeshBasicMaterial({ wireframe: true, color: 0x00ff00 }),
-    )
+      new MeshLambertMaterial({ wireframe: true, color: 0x00ff00 }),
+    );
     this.cylinder1.rotation.x = Math.PI / 2;
     this.cylinder1.rotation.z = Math.PI / 2;
     this.scene.add(this.cylinder1);
     this.camera.position.z = 5;
+
+    loader.load(
+      slotBackground,
+      (texture) => {
+        const geometry = new BoxGeometry(12, 8, 0.1)
+        const material = new MeshLambertMaterial({
+          map: texture,
+        });
+        this.scene.add(new Mesh(geometry, material));
+      },
+    );
   }
 
   update(delta: number) {
